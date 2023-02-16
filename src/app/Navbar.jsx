112c336f431ff7f4ca22c300ca00f1522b717966
colespen <spencerlewiscole@gmail.com>
2023-02-16 13:34:23 -0500

    add basic nav css

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,6 +1,15 @@
-import { Link } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { fetchNotifications } from '../features/notifications/notificationsSlice';
 
 export const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const fetchNewNotifications = () => {
+    dispatch(fetchNotifications());
+  };
+
   return (
     <nav>
       <section>
@@ -10,7 +19,11 @@ export const Navbar = () => {
           <div className="navLinks">
             <Link to="/">Posts</Link>
             <Link to="/users">Users</Link>
+            <Link to="/notifications">Notifications</Link>
           </div>
+          <button className="button" onClick={fetchNewNotifications}>
+            Notifications <strong>Refresh</strong>
+          </button>
         </div>
       </section>
     </nav>
